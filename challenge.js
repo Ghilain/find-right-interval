c8b8a21e708b7c1c04d1c3b2cfdc5eb06e18bd66
Ghilain <ishimweghilain5050@gmail.com>
2023-01-12 16:47:19 +0200

    add function find mi value

--- challenge.js
+++ challenge.js
@@ -8,5 +8,16 @@
 const start_index_arr = intervals
   .map((e, i) => [e[0], i])
   .sort((a, b) => a[0] - b[0]);
- };
+  const getEleStartValue = (i) => start_index_arr[i][0]; // to compare to target
+
+};
+
 const lessThan = (a, b) => a < b; // bs & lb // 4ge
+const blu_midValFromFunc = (compare, getArrVal, target, left, right) => {
+while (left < right) {
+  const mid = left + Math.trunc((right - left) / 2);
+  if (compare(getArrVal(mid), target)) left = mid + 1;
+  else right = mid;
+}
+return compare(getArrVal(left), target) ? left + 1 : left;
+};

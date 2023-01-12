58063df9f7b30602f55c8a8e3995ead645fa74e3
Ghilain <ishimweghilain5050@gmail.com>
2023-01-12 16:43:15 +0200

    decrale variable to hold length and one for index arr

--- challenge.js
+++ challenge.js
@@ -3,5 +3,11 @@
  * @return {number[]}
  */
  var findRightInterval = function(intervals) {
+    const n = intervals.length;
 
-};
+const start_index_arr = intervals
+  .map((e, i) => [e[0], i])
+  .sort((a, b) => a[0] - b[0]);
+const getEleStartValue = (i) => start_index_arr[i][0]; // to compare to target
+
+ };

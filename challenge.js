30f323a226610d500b58734ad19a1a2c091170a0
Ghilain <ishimweghilain5050@gmail.com>
2023-01-12 16:49:27 +0200

    final code

--- challenge.js
+++ challenge.js
@@ -8,13 +8,14 @@
 const start_index_arr = intervals
   .map((e, i) => [e[0], i])
   .sort((a, b) => a[0] - b[0]);
-  const getEleStartValue = (i) => start_index_arr[i][0]; // to compare to target
-  const ans = [];
-  for (let [, end] of intervals) {
-  const idx = blu_midValFromFunc(lessThan, getEleStartValue, end, 0, start_index_arr.length - 1);
-    ans.push(idx === n ? -1 : start_index_arr[idx][1]);
-  }
-  return ans;
+const getEleStartValue = (i) => start_index_arr[i][0]; // to compare to target
+
+const ans = [];
+for (let [, end] of intervals) {
+const idx = blu_midValFromFunc(lessThan, getEleStartValue, end, 0, start_index_arr.length - 1);
+  ans.push(idx === n ? -1 : start_index_arr[idx][1]);
+}
+return ans;
 };
 
 const lessThan = (a, b) => a < b; // bs & lb // 4ge
